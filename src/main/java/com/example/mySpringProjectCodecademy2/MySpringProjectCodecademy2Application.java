48510c09e996525c34bfc2346014a2d9bc6bf676
Eric Vo <eric.vo215@gmail.com>
2023-10-15 12:18:04 -0400

    initial push

---
+++ src/main/java/com/example/mySpringProjectCodecademy2/MySpringProjectCodecademy2Application.java
@@ -0,0 +1,13 @@
+package com.example.mySpringProjectCodecademy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringProjectCodecademy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringProjectCodecademy2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mySpringProjectCodecademy2/MySpringProjectCodecademy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mySpringProjectCodecademy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringProjectCodecademy2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
